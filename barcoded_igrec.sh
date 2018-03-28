6ef0b635d94b51519e33da976ef47e95c86b6534
olgabot <olga.botvinnik@gmail.com>
2018-03-28 14:51:52 -0700

    update first10k to use separate output file

--- barcoded_igrec.sh
+++ barcoded_igrec.sh
@@ -1,8 +1,8 @@
 sudo python2.7 /home/ubuntu/ig_repertoire_constructor/barcoded_igrec.py \
      -1 /mnt/data/krista/rawdata/IgSeqBX1_S1_R1_001_first10k.fastq \
      -2 /mnt/data/krista/rawdata/IgSeqBX1_S1_R2_001_first10k.fastq \
-     --output /mnt/data/krista/barcoded_igrec --loci IGH \
-     > >(tee -a barcoded_igrec.sh.out) 2> >(tee -a barcoded_igrec.sh.err >&2)
+     --output /mnt/data/krista/barcoded_igrec_first10k --loci IGH \
+     > >(tee -a barcoded_igrec_first10k.sh.out) 2> >(tee -a barcoded_igrec_first10k.sh.err >&2)
 
 
 #sudo python2.7 /home/ubuntu/ig_repertoire_constructor/barcoded_igrec.py \
