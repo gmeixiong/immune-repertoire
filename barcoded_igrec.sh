52a0eba670c63119fe9ed73d344365a817e59628
olgabot <olga.botvinnik@gmail.com>
2018-03-28 21:52:35 +0000

    fix merge conflict

--- barcoded_igrec.sh
+++ barcoded_igrec.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo python2.7 /home/ubuntu/ig_repertoire_constructor/barcoded_igrec.py \
      -1 /mnt/data/krista/rawdata/IgSeqBX1_S1_R1_001_first10k.fastq \
      -2 /mnt/data/krista/rawdata/IgSeqBX1_S1_R2_001_first10k.fastq \
@@ -10,4 +12,3 @@ sudo python2.7 /home/ubuntu/ig_repertoire_constructor/barcoded_igrec.py \
 #     -2 /mnt/data/krista/rawdata/IgSeqBX1_S1_R2_001.fastq \
 #     --output /mnt/data/krista/barcoded_igrec --loci IGH \
 #     > >(tee -a barcoded_igrec.sh.out) 2> >(tee -a barcoded_igrec.sh.err >&2)
-
