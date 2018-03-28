db76547b153331f56f5abc0bd9885add2ac045d1
olgabot <olga.botvinnik@gmail.com>
2018-03-28 21:55:03 +0000

    explicitly use env python2.7

--- barcoded_igrec.sh
+++ barcoded_igrec.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo python2.7 /home/ubuntu/ig_repertoire_constructor/barcoded_igrec.py \
+sudo /home/ubuntu/anaconda/envs/python2.7-env/bin/python2.7 /home/ubuntu/ig_repertoire_constructor/barcoded_igrec.py \
      -1 /mnt/data/krista/rawdata/IgSeqBX1_S1_R1_001_first10k.fastq \
      -2 /mnt/data/krista/rawdata/IgSeqBX1_S1_R2_001_first10k.fastq \
      --output /mnt/data/krista/barcoded_igrec_first10k --loci IGH \
      > >(tee -a barcoded_igrec_first10k.sh.out) 2> >(tee -a barcoded_igrec_first10k.sh.err >&2)
 
 
-#sudo python2.7 /home/ubuntu/ig_repertoire_constructor/barcoded_igrec.py \
+#sudo /home/ubuntu/anaconda/envs/python2.7-env/bin/python2.7 /home/ubuntu/ig_repertoire_constructor/barcoded_igrec.py \
 #     -1 /mnt/data/krista/rawdata/IgSeqBX1_S1_R1_001.fastq \
 #     -2 /mnt/data/krista/rawdata/IgSeqBX1_S1_R2_001.fastq \
 #     --output /mnt/data/krista/barcoded_igrec --loci IGH \
