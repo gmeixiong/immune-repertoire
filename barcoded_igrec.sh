e9a91cb933fdbe4e150d66093ebf0f1905709941
olgabot <olga.botvinnik@gmail.com>
2018-03-28 14:45:07 -0700

    Add saving of both stderr and stdout

--- barcoded_igrec.sh
+++ barcoded_igrec.sh
@@ -1,4 +1,6 @@
 sudo python2.7 /home/ubuntu/ig_repertoire_constructor/barcoded_igrec.py \
      -1 /mnt/data/krista/rawdata/IgSeqBX1_S1_R1_001.fastq \
      -2 /mnt/data/krista/rawdata/IgSeqBX1_S1_R2_001.fastq \
-     --output /mnt/data/krista/barcoded_igrec --loci IGH
+     --output /mnt/data/krista/barcoded_igrec --loci IGH \
+     > >(tee -a barcoded_igrec.sh.out) 2> >(tee -a barcoded_igrec.sh.err >&2)
+
