abbcb86341046ab33855dc4b811085460a3ced67
olgabot <olga.botvinnik@gmail.com>
2018-03-28 15:18:13 -0700

    Add data dir

--- remote.sh
+++ remote.sh
@@ -30,30 +30,32 @@ cd krista
 sudo aws s3 cp --recursive \
   s3://czbiohub-seqbot/fastqs/180128_M05295_0078_000000000-BJNBT .
 
+DATA_DIR=/mnt/data/krista/rawdata
+
 
 # Make subset of first 10k reads
-sudo head -n 40000 IgSeqBX1_S1_R1_001.fastq > IgSeqBX1_S1_R1_001_first10k.fastq
-sudo head -n 40000 IgSeqBX1_S1_R2_001.fastq > IgSeqBX1_S1_R2_001_first10k.fastq
+sudo head -n 40000 $DATA_DIR/IgSeqBX1_S1_R1_001.fastq > $DATA_DIR/IgSeqBX1_S1_R1_001_first10k.fastq
+sudo head -n 40000 $DATA_DIR/IgSeqBX1_S1_R2_001.fastq > $DATA_DIR/IgSeqBX1_S1_R2_001_first10k.fastq
 
-./run_barcoded_igrec.py IgSeqBX1_S1_R1_001_first10k.fastq \
-    IgSeqBX1_S1_R2_001_first10k.fastq barcoded_igrec_first10k
+./run_barcoded_igrec.py $DATA_DIR/IgSeqBX1_S1_R1_001_first10k.fastq \
+    $DATA_DIR/IgSeqBX1_S1_R2_001_first10k.fastq barcoded_igrec_first10k
 
 # Make subset of first 100k reads
-sudo head -n 400000 IgSeqBX1_S1_R1_001.fastq > IgSeqBX1_S1_R1_001_first100k.fastq
-sudo head -n 400000 IgSeqBX1_S1_R2_001.fastq > IgSeqBX1_S1_R2_001_first100k.fastq
+sudo head -n 400000 $DATA_DIR/IgSeqBX1_S1_R1_001.fastq > $DATA_DIR/IgSeqBX1_S1_R1_001_first100k.fastq
+sudo head -n 400000 $DATA_DIR/IgSeqBX1_S1_R2_001.fastq > $DATA_DIR/IgSeqBX1_S1_R2_001_first100k.fastq
 
-./run_barcoded_igrec.py IgSeqBX1_S1_R1_001_first100k.fastq \
-    IgSeqBX1_S1_R2_001_first100k.fastq barcoded_igrec_first100k
+./run_barcoded_igrec.py $DATA_DIR/IgSeqBX1_S1_R1_001_first100k.fastq \
+    $DATA_DIR/IgSeqBX1_S1_R2_001_first100k.fastq barcoded_igrec_first100k
 
 # Make subset of first 1 million reads
-sudo head -n 4000000 IgSeqBX1_S1_R1_001.fastq > IgSeqBX1_S1_R1_001_first1M.fastq
-sudo head -n 4000000 IgSeqBX1_S1_R2_001.fastq > IgSeqBX1_S1_R2_001_first1M.fastq
+sudo head -n 4000000 $DATA_DIR/IgSeqBX1_S1_R1_001.fastq > $DATA_DIR/IgSeqBX1_S1_R1_001_first1M.fastq
+sudo head -n 4000000 $DATA_DIR/IgSeqBX1_S1_R2_001.fastq > $DATA_DIR/IgSeqBX1_S1_R2_001_first1M.fastq
 
-./run_barcoded_igrec.py IgSeqBX1_S1_R1_001_first1M.fastq \
-    IgSeqBX1_S1_R2_001_first1M.fastq barcoded_igrec_first1M
+./run_barcoded_igrec.py $DATA_DIR/IgSeqBX1_S1_R1_001_first1M.fastq \
+    $DATA_DIR/IgSeqBX1_S1_R2_001_first1M.fastq barcoded_igrec_first1M
 
 
 source activate python2.7-env
 /home/ubuntu/ig_repertoire_constructor/barcoded_igrec.py \
-  -1 IgSeqBX1_S1_R1_001.fastq -2 IgSeqBX1_S1_R2_001.fastq \
+  -1 $DATA_DIR/IgSeqBX1_S1_R1_001.fastq -2 $DATA_DIR/IgSeqBX1_S1_R2_001.fastq \
   --output barcoded_igrec --loci 'all BCRs'
