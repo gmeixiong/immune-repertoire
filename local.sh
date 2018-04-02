3f1bf1945f2a4d851f46ba745c5a803b6f6a566f
olgabot <olga.botvinnik@gmail.com>
2018-04-02 11:35:31 -0700

    add vanilla ssh command for logging in

--- local.sh
+++ local.sh
@@ -2,4 +2,5 @@
 
 
 aegea launch --iam-role S3fromEC2 --ami-tags Name=czbiohub-miniconda -t m4.4xlarge  olgabot-igrec-m4
-aegea ssh ubuntu@olgabot-igrec-m4
+# aegea ssh ubuntu@olgabot-igrec-m4
+ssh -i ~/.ssh/aegea.launch.olgabot.Olgas-MacBook-Pro.pem ubuntu@ec2-34-210-67-18.us-west-2.compute.amazonaws.com
