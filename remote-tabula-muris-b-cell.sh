ecba4919c4b30b034c9a8c940bea66859bd79a95
olgabot <olga.botvinnik@gmail.com>
2018-04-05 15:16:23 -0700

    add tabula muris bcell fastqs

---
+++ remote-tabula-muris-b-cell.sh
@@ -0,0 +1,2 @@
+cd /mnt/data
+aws s3 sync s3://czbiohub-maca/bcell_fastqs/ .
--- run_igrec_many_fastqs.py
+++ run_igrec_many_fastqs.py
@@ -1,3 +1,5 @@
+
+import StringIO
 import os
 
 import click
@@ -5,14 +7,51 @@ import click
 import run_igrec
 
 
+# Stolen from http://www.genomearchitecture.com/2014/01/how-to-gunzip-on-the-fly-with-python
+class gzopen(object):
+   """Generic opener that decompresses gzipped files
+   if needed. Encapsulates an open file or a GzipFile.
+   Use the same way you would use 'open()'.
+   """
+   def __init__(self, fname):
+      f = open(fname)
+      # Read magic number (the first 2 bytes) and rewind.
+      magic_number = f.read(2)
+      f.seek(0)
+      # Encapsulated 'self.f' is a file or a GzipFile.
+      if magic_number == '\x1f\x8b':
+         self.f = gzip.GzipFile(fileobj=f)
+      else:
+         self.f = f
+
+   # Define '__enter__' and '__exit__' to use in
+   # 'with' blocks. Always close the file and the
+   # GzipFile if applicable.
+   def __enter__(self):
+      return self
+   def __exit__(self, type, value, traceback):
+      try:
+         self.f.fileobj.close()
+      except AttributeError:
+         pass
+      finally:
+         self.f.close()
+
+   # Reproduce the interface of an open file
+   # by encapsulation.
+   def __getattr__(self, name):
+      return getattr(self.f, name)
+   def __iter__(self):
+      return iter(self.f)
+   def next(self):
+      return next(self.f)
+
+
 @click.command()
 @click.argument('fastqs', nargs=-1)
-@click.argument('output_prefix')
+@click.argument('output_prefix10')
 @click.option('--barcoded', is_flag=True)
 def cli(fastqs, output_prefix, barcoded):
-    output_prefix = os.path.abspath(output_prefix)
-
-    igrec = 'BarcodedIgReC' if barcoded else 'IgReC'
 
     for fastq in fastqs:
         # Only operate on R1s to avoid duplication
@@ -21,10 +60,8 @@ def cli(fastqs, output_prefix, barcoded):
         read1 = fastq
         read2 = fastq.replace('_R1_', '_R2_')
 
-        subfolder = os.path.basename(read1).split('_R1_')[0]
+        subfolder = os.path.basename(read1).split('.fastq')[0]
         output_folder = os.path.join(output_prefix, subfolder)
-        click.echo(f'Running {igrec} on {read1} and {read2}, '
-                   f'writing to {output_folder}')
 
         run_igrec.cli(read1, read2, output_folder, barcoded)
 
