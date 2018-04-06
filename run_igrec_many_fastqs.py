3495b480ca8e5af49ea5238c86034865723ce4ac
olgabot <olga.botvinnik@gmail.com>
2018-04-05 17:48:20 -0700

    Update print statements

--- run_igrec_many_fastqs.py
+++ run_igrec_many_fastqs.py
@@ -10,19 +10,18 @@ from run_igrec import run_igrec
 @click.argument('output_prefix')
 @click.option('--barcoded', is_flag=True)
 def cli(fastqs, output_prefix, barcoded):
-    print(fastqs)
-
     for fastq in fastqs:
         # Only operate on R1s to avoid duplication
         if "R2" in fastqs:
             continue
         read1 = fastq
-        print(read1)
+        print(f'read1: {read1}')
         read2 = fastq.replace('_R1_', '_R2_')
 
         subfolder = os.path.basename(read1).split('_R1_')[0]
-        print(subfolder)
+        print(f'subfolder: {subfolder}')
         output_folder = os.path.join(output_prefix, subfolder)
+        print(f'output_folder: {output_folder}')
 
         run_igrec(read1, read2, output_folder, barcoded)
 
