b040f75cfd70ae2c2755e0f63cb7f8f1b75eddae
olgabot <olga.botvinnik@gmail.com>
2018-04-12 13:47:48 -0700

    initial commit for formatting barcodes in GO

--- format_barcodes.go
+++ format_barcodes.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+  "io"
+  "github.com/biogo/biogo/io/seqio/fastq"
+  "os"
+  "github.com/biogo/biogo/seq/linear"
+  "github.com/biogo/biogo/alphabet"
+  "fmt"
+)
+
 func main() {
-	
+  // Open the fastq file specified on the command line
+  // for reading:
+  fh, err := os.Open(os.Args[1])
+  // Check for open errors and abort:
+  if err != nil {
+	panic(err)
+  }
+
+  // Create a template sequence for the reader:
+  template := linear.NewQSeq("", alphabet.QLetters{}, alphabet.DNA, alphabet.Sanger)
+  // Create a fastq reader:
+  reader := fastq.NewReader(fh, template)
+
+  for {
+	// Read the next record:
+	seq, err := reader.Read()
+	// Break loop if we reached the end of the file:
+	if err == io.EOF {
+	  break
+	}
+	fmt.Println(seq.CloneAnnotation().Desc)
+  }
 }
