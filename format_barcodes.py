3fcafc88aa7d1388b23f9f0c3c320a75021a32c8
olgabot <olga.botvinnik@gmail.com>
2018-04-12 13:27:32 -0700

    Add option to print timestamp to stderr

--- format_barcodes.py
+++ format_barcodes.py
@@ -2,6 +2,7 @@
 Formats pRESTO primer-pass_pair-pass.fastq barcodes for IgReC
 """
 
+import datetime
 import re
 import sys
 
@@ -19,19 +20,27 @@ def igrecify_barcode(record, read_number, pattern):
     return record
 
 
+def timestamp():
+    return str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+
 @click.command()
 @click.argument('fastq', type=click.File())
 @click.argument('read_number', type=click.IntRange(1, 2))
-def cli(fastq, read_number):
+@click.argument('--timestamp', flag=True, default=False)
+def cli(fastq, read_number, timestamp):
     """Converts presto-barcoded fastqo to igrec-formatted"""
 
     # print(fastq)
+    if timestamp:
+        click.echo(timestamp(), err=True)
     pattern = re.compile(
         'BARCODE=(?P<r1_barcode>[ACGT]+),(?P<r2_barcode>[ACGT]+)')
     reformatted_records = (igrecify_barcode(record, read_number, pattern)
                            for record in SeqIO.parse(fastq, "fastq"))
-
     SeqIO.write(reformatted_records, sys.stdout, 'fastq')
+    if timestamp:
+        click.echo(timestamp(), err=True)
 
 
 if __name__ == '__main__':
